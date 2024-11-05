133e2a78702eb1b7bf1fdaf46538a81b68a1af26
KC10201 <70864069+KC10201@users.noreply.github.com>
2024-11-04 19:57:06 -0800

    Create create_graph.py
    combine into single function to generate full graph representation of sumo traffic signal system.

---
+++ graph/create_graph.py
@@ -0,0 +1,21 @@
+from .graph_converter import construct_graph_representation
+from .node_features import batch_traffic_signal_feature
+from ..sumo_rl.environment.traffic_signal import TrafficSignal
+
+def construct_graph_and_features(ts_list, device):
+  '''
+  Returns:
+    * node_features (Tensor): list of feature vectors for each node.
+    * adj_list (Tensor): list of directed edges in graph.
+    * ts_indx (dict[str: int]): mapping of TrafficSignal id to node index.
+    * lane_indx (dict[str: int]): mapping of lane id to adj_list index.
+    * num_nodes (int): Total number of nodes in graph.
+
+  Args:
+    * ts_list (list[TrafficSignal]): list of TrafficSignals.
+    * device (str): Type of device for Tensor.
+  '''
+  ts_indx, num_nodes, lane_indx, adj_list = construct_graph_representation(ts_list, device)
+  node_features = batch_traffic_signal_feature(ts_list, ts_indx, num_nodes, device)
+
+  return node_features, adj_list, ts_indx, lane_indx, num_nodes
