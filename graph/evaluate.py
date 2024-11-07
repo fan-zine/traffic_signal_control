039066b5639884dcb04887f88c027738b53b9bef
KC10201 <70864069+KC10201@users.noreply.github.com>
2024-11-06 22:07:10 -0800

    Create evaluate.py

---
+++ graph/evaluate.py
@@ -0,0 +1,21 @@
+
+
+def evaluate(env, model, epochs, return_all_rewards):
+  '''
+  Evaluate model by running  model over x epochs and return average reward across all epochs, and list of reward per epoch if requested.
+
+  Args:
+    model: Model to evaluate
+    epochs (int): number of epochs to run model over
+  '''
+  reward = []
+
+  observations = env.reset()
+  while env.agents:
+    actions = model(env)
+    observations, rewards, terminations, truncations, infos = env.step(actions)
+    reward.append(np.mean([rewards[ts] for ts in rewards))
+
+  if return_all_rewards:
+    return np.mean(reward), reward
+  return np.mean(reward)
