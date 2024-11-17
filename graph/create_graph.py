f5cb72fd91ce713eadc3c35c7731d00d025234d0
KC10201 <70864069+KC10201@users.noreply.github.com>
2024-11-16 18:11:46 -0800

    Update create_graph.py
    Add function to generate subgraphs for local nieghbors.

--- graph/create_graph.py
+++ graph/create_graph.py
@@ -2,6 +2,8 @@ from .graph_converter import construct_graph_representation
 from .node_features import batch_traffic_signal_feature
 from ..sumo_rl.environment.traffic_signal import TrafficSignal
 
+import torch_geometric
+
 def construct_graph_and_features(ts_list, device):
   '''
   Returns:
@@ -19,3 +21,28 @@ def construct_graph_and_features(ts_list, device):
   node_features = batch_traffic_signal_feature(ts_list, ts_indx, num_nodes, device)
 
   return node_features, adj_list, ts_indx, lane_indx, num_nodes
+
+
+def get_neighbors(ts_indx, adj_list, k):
+  '''
+  Use k_hop_subgraph (https://pytorch-geometric.readthedocs.io/en/latest/modules/utils.html#torch_geometric.utils.k_hop_subgraph).
+  Returns dictionary with tuple representing subgraph:
+    * subset (LongTensor): the nodes involved in the subgraph
+    * edge_index (LongTensor): the filtered edge_index connectivity
+    * mapping (LongTensor): the mapping from node indices in node_idx to their new location
+    * edge_mask (LongTensor): edge mask indicating which edges were preserved
+  
+  Args:
+    * ts_indx (dict[str: int]): mapping of TrafficSignal id to node index.
+    * adj_list (Tensor): list of directed edges in graph.
+    * k (int): value of k, for k-hop neighbors.
+  '''
+
+  neighbors = {ts_id: torch_geometric.utils.k_hop_subgraph(node_idx=node_index, 
+                                                           num_hops=k, 
+                                                           edge_index=adj_list, 
+                                                           relabel_nodes=False) \
+               for ts_id, node_index in ts_indx.items()}
+
+  return neighbors
+
