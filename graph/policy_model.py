963474bb4657350e6805db4047863bd58e533082
KC10201 <70864069+KC10201@users.noreply.github.com>
2024-11-23 21:49:16 -0800

    Update policy_model.py

--- graph/policy_model.py
+++ graph/policy_model.py
@@ -2,6 +2,13 @@ import torch
 
 class PolicyNetwork(torch.nn.Module):
   def __init__(self, ts_map, traffic_system, args, aggr="mean"):
+        '''
+        Args:
+        * ts_map (Dict{str: int}): mapping of traffic signal id to node index
+        * traffic_system (Tensor[2,|E|]): adjacency list of entire traffic system
+        * args (dict): arguments relating to traffic system. e.i: eigenvectors.
+        * aggr (str): type of aggregation to use.
+        '''
         super(PolicyNetwork, self).__init__()
         self.traffic_system = traffic_system
         self.ts_map = ts_map
